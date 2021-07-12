1167ae10cb1d80e273e6d6e748bec8864b97fb49
teja9949 <67798221+teja9949@users.noreply.github.com>
2021-07-12 11:46:37 +0530

    Delete tobrot.config directory

--- tobrot.config/Config.py
+++
@@ -1,7 +0,0 @@
-from tobrot.config import Config
-
-class Config(Config):
-  TG_BOT_TOKEN = "1821165709:AAGY7JDZuVW0x1BOhKd34q11FkM1tJnXDNQ"
-  APP_ID = 6666012
-  API_HASH = "1243fe9e54e1e6ec52d67960f1e4ed6d"
-  AUTH_CHANNEL = -1001563645757
